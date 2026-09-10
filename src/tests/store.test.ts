2e049ee6633fd5ae0af6b44325cc9871c4e7c2c0
Marcus Gasberg <maga@systematic.com>
2026-09-10 14:27:35 +0200

    feat m6: reject stale reorders with optimistic concurrency

--- src/tests/store.test.ts
+++ src/tests/store.test.ts
@@ -239,7 +239,7 @@ describe("reorderTask", () => {
         a.taskId,
       ]);
 
-      yield* apply(reorderMutation(a.taskId, -5, 1));
+      yield* apply(reorderMutation(a.taskId, -5, 4));
       expect((yield* getTasks()).map((task) => task.id)).toEqual([
         a.taskId,
         b.taskId,
